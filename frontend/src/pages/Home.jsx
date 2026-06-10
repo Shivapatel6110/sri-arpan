d3f650a0206d8d693d26e4dcded2623032c43ca2
Shivapatel6110 <Shivapatel6110@gmail.com>
2026-06-10 10:59:50 +0530

    Updated Chadhava CRUD

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import Navbar from "../components/Navbar";
+import { Link } from "react-router-dom";
 import Hero from "../components/Hero";
 import Services from "../components/Services";
 import FeaturedPujas from "../components/FeaturedPujas";
@@ -13,6 +14,7 @@ function Home() {
   return (
     <>
       <Navbar />
+      <Link to="/admin/chadhava">Admin</Link>
       <Hero />
       <Services />
       <FeaturedPujas />
